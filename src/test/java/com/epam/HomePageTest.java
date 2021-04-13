7c7c40518397356c9a98b2e5c014dcb02e950744
Redbutton18 <LenovoP780>
2021-04-13 10:25:23 +0300

    Initial commit

---
+++ src/test/java/com/epam/HomePageTest.java
@@ -0,0 +1,41 @@
+package com.epam;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+public class HomePageTest {
+
+    public String baseUrl = "https://epam.com/";
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("firefox");
+        capabilities.setVersion("86.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+
+        driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(),
+                capabilities);
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void visitHomePage() {
+        driver.get(baseUrl);
+        WebElement message = driver.findElement(By.className("title-slider__slide-row"));
+        Assert.assertTrue(message.isDisplayed());
+        driver.close();
+    }
+}
